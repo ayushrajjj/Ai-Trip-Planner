9a4bb0e344f54d728665bae91102e9f154a677a0
ayush raj <vermaayushraj987@gmail.com>
2025-04-26 02:43:24 +0530

    firebase updation

--- src/service/firebaseConfig.jsx
+++ src/service/firebaseConfig.jsx
@@ -1,17 +1,17 @@
-// firebaseConfig.js (or any other name you'd like)
+// firebaseConfig.js
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 // Your Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDaV15N_jQja_sGeBelt7Sawt33dMajrOg",
-  authDomain: "ai-trip-planner-43dcd.firebaseapp.com",
-  projectId: "ai-trip-planner-43dcd",
-  storageBucket: "ai-trip-planner-43dcd.appspot.com",
-  messagingSenderId: "563456730780",
-  appId: "1:563456730780:web:4cf38384faaf0d475eef59",
-  measurementId: "G-FGHP0H8C30"
+  apiKey: "AIzaSyDaV15N_jQja_sGeBelt7Sawt33dMajrOg", // Replace with your Firebase API key
+  authDomain: "ai-trip-planner-43dcd.firebaseapp.com", // Replace with your Firebase Auth domain
+  projectId: "ai-trip-planner-43dcd", // Replace with your Firebase project ID
+  storageBucket: "ai-trip-planner-43dcd.appspot.com", // Replace with your Firebase storage bucket
+  messagingSenderId: "563456730780", // Replace with your Firebase messaging sender ID
+  appId: "1:563456730780:web:4cf38384faaf0d475eef59", // Replace with your Firebase app ID
+  measurementId: "G-FGHP0H8C30" // Replace with your Firebase measurement ID
 };
 
 // Initialize Firebase
